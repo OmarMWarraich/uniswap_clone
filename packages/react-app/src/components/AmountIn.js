612e38125a77fb4f3217ca23afec3a02be13411f
OmarMWarraich <earthling986@gmail.com>
2022-09-24 16:26:05 +0500

    feat: Add props, useRef in AmountIn component

--- packages/react-app/src/components/AmountIn.js
+++ packages/react-app/src/components/AmountIn.js
@@ -1,54 +1,67 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from "react";
 
 import { chevronDown } from "../assets";
-import styles from '../styles';
-import { useOnClickOutside } from '../utils';
+import { useOnClickOutside } from "../utils";
+import styles from "../styles";
 
-const AmountIn = () => {
-    const [showList, setShowList] = useState(false);
+const AmountIn = ({ value, onChange, currencyValue, onSelect, currencies, isSwapping }) => {
+  const [showList, setShowList] = useState(false);
+  const [activeCurrency, setActiveCurrency] = useState("Select");
+  const ref = useRef()
 
-    return (
-        <div className={styles.amountContainer}>
-            <input
-                placeholder="0.0"
-                type="number"
-                value=""
-                disabled={false}
-                onChange={() => {}}
-                className={styles.amountInput}
-            />
+  useOnClickOutside(ref, () => setShowList(false))
 
-            <div className="relative" onClick={() => setShowList
-                ((prevState) => !prevState)}>
-                <button className={styles.currencyButton}>
-                    {"ETH"}
-                    <img
-                      src={chevronDown}
-                      alt="chevron-down"
-                      className={`w-4 h-4 object-contain ml-2 ${showList ?
-                      'rotate-180' : 'rotate-0'}`}
-                    />
-                </button>
+  useEffect(() => {
+    if (Object.keys(currencies).includes(currencyValue))
+      setActiveCurrency(currencies[currencyValue]);
+    else setActiveCurrency("Select");
+  }, [currencies, currencyValue]);
 
-                {showList && (
-                    <ul className={styles.currencyList}>
-                        {[
-                            { token: "ETH", tokenName: "ETH" },
-                            { token: "OVA GOLD", tokenName: "OVA GOLD" },
-                        ].map(({token, tokenName}, index) => (
-                            <li
-                                key={index}
-                                className={`${styles.currencyListItem} ${true ?
-                                'bg-site-dim2' : ''} cursor-pointer`}
-                            >
-                              {tokenName}                
-                            </li>        
-                        ))}
-                    </ul>
-                )}  
-            </div>    
-        </div> 
-    )
-    }
+  return (
+    <div className={styles.amountContainer}>
+      <input
+        placeholder="0.0"
+        type="number"
+        value={value}
+        disabled={isSwapping}
+        onChange={(e) => typeof onChange === "function" && onChange(e.target.value)}
+        className={styles.amountInput}
+      />
+
+      <div className="relative" onClick={() => setShowList(!showList)}>
+        <button className={styles.currencyButton}>
+          {activeCurrency}
+          <img
+            src={chevronDown}
+            alt="cheveron-down"
+            className={`w-4 h-4 object-contain ml-2 ${
+              showList ? "rotate-180" : "rotate-0"
+            }`}
+          />
+        </button>
+
+        {showList && (
+          <ul ref={ref} className={styles.currencyList}>
+            {Object.entries(currencies).map(([token, tokenName], index) => (
+              <li
+                key={index}
+                className={`${styles.currencyListItem} ${
+                  activeCurrency === tokenName ? "bg-site-dim2" : ""
+                } cursor-pointer`}
+                onClick={() => {
+                  if (typeof onSelect === "function") onSelect(token);
+                  setActiveCurrency(tokenName);
+                  setShowList(false);
+                }}
+              >
+                {tokenName}
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default AmountIn;
