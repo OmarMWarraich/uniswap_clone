5e60edba4566225906b5f3501e28e2ac16a9bffd
OmarMWarraich <earthling986@gmail.com>
2022-09-24 16:29:02 +0500

    fix: Add props Balance component

--- packages/react-app/src/components/Balance.js
+++ packages/react-app/src/components/Balance.js
@@ -1,23 +1,23 @@
-import React from 'react';
+import React from "react";
 import { formatUnits, parseUnits } from "ethers/lib/utils";
 
-import styles from '../styles';
+import styles from "../styles";
 
-const Balance = () => {
-    const tokenBalance = parseUnits("0.00251");
-
-    return (
-        <div className={styles.balance}>
-            <p className={styles.balanceText}>
-                { tokenBalance && (
-                    <>
-                        <span className={styles.balanceBold}>Balance: </span>
-                        {formatUnits(tokenBalance || parseUnits("0"))}
-                    </>    
-                )}
-            </p>
-        </div> 
-    )
-    }
+const Balance = ({ tokenBalance }) => {
+  return (
+    <div className={styles.balance}>
+      <p className={styles.balanceText}>
+        {tokenBalance ? (
+          <>
+            <span className={styles.balanceBold}>Balance: </span>
+            {formatUnits(tokenBalance ?? parseUnits("0"))}
+          </>
+        ) : (
+          ""
+        )}
+      </p>
+    </div>
+  );
+};
 
 export default Balance;
